62e2f2216996f38736244ab848e4f138ccfe13d0
taras42 <taras.my.mail@gmail.com>
2016-08-21 22:17:53 +0300

    added shadow light frustum

--- js/scene/object/lights.js
+++ js/scene/object/lights.js
@@ -6,6 +6,16 @@ app.createLights = function () {
     var shadowLight = new THREE.DirectionalLight(0xf7d9aa, 1.5);
     shadowLight.castShadow = true;
 
+	shadowLight.shadow.camera.left = -12;
+	shadowLight.shadow.camera.right = 12;
+	shadowLight.shadow.camera.top = 12;
+	shadowLight.shadow.camera.bottom = -12;
+	shadowLight.shadow.camera.near = 1;
+	shadowLight.shadow.camera.far = 200;
+
+	shadowLight.shadow.mapSize.width = 2048;
+	shadowLight.shadow.mapSize.height = 2048;
+
     shadowLight.position.set(100, 100, 70);
 
     return {
